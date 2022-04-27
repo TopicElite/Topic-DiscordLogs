31518d28c521ffd9e2728e634276478f0c5f35b0
TopicElite <topicelite0070@gmail.com>
2022-04-27 19:15:28 +0200

    Bug Fix
    Important bugfix + fixed error message that comes when typing in the console.

--- server/server.custom.lua
+++ server/server.custom.lua
@@ -3,17 +3,17 @@
 function runResource()
     local allResourcesAreStarted = ""
     for k,v in ipairs(Config.CustomLog) do
-        local resourceName = "7DiscordLog:" + v.serverEventName
+        local resourceName = "7DiscordLog:" .. v.serverEventName
         allResourcesAreStarted = allResourcesAreStarted .. resourceName .. " | "
         RegisterNetEvent(resourceName)
         AddEventHandler(resourceName, function(title, description)
-            sendDiscordLog(v.embedColor, v.webhook, title, description)
+            sendCustomDiscordLog(v.embedColor, v.webhook, title, description)
         end)
     end
     print("All CustomLogs are loaded: " .. allResourcesAreStarted)
 end
 
-function sendDiscordLog(embedColor, webhook, title, description)
+function sendCustomDiscordLog(embedColor, webhook, title, description)
 
     PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({
         username = Config.username, 
