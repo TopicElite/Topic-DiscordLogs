23ab69e8aec44a8d9ddc9fb6af4729a5dd18320a
BadFoxrp <123276846+BadFoxrp@users.noreply.github.com>
2023-09-18 01:54:15 +0200

    Create lt.lua
    add locales translated to Lithuania languange

---
+++ locales/lt.lua
@@ -0,0 +1,54 @@
+Locales['lt'] = {
+    ['konsolė'] = 'konsole',
+
+    ------ Actions ------
+    ['Add'] = 'Pridėti',
+    ['Remove'] = 'Pašalinti',
+    ['Set'] = 'Nustatyti',
+    ['Sell'] = 'Parduoti',
+    ['Harvest'] = 'Harvest',
+    ['Processed'] = 'Apdorota',
+    ['Wash'] = 'Nuplaukite',
+    ['Starting'] = 'Pradedama',
+    ['Start'] = 'Prasidėjo',
+    ['Stop'] = 'Sustabdyta',
+    ['Revive'] = 'Atgaivinkite',
+    ['Heal'] = 'Išgyti',
+    ['Hire'] = 'Samdyti',
+    ['Promote'] = 'Paaukštinti',
+    ['Fire'] = 'Ugnis',
+    
+    ------ Resource Log ------
+    ['resourcelog_title'] = '%s',
+    ['resourcelog_description'] = '%s: %s',
+
+    ------ Command Log ------
+    ['commandslog_description'] = 'Command: %s %s',
+
+    ------ Items Log ------
+    ['itemslog_description'] = '%s: %sx %s (%s)',
+
+    ------ Money Log ------
+    ['money'] = 'Cash',
+    ['black_money'] = 'Black money',
+    ['bank'] = 'Bank',
+    ['moneylog_description'] = '%s: $%s %s (%s) | Current state: $%s',
+
+    ------ Weapon Log ------
+    ['weaponlog_description_weapon'] = '%s: %s (%s) with %sx Ammo',
+    ['weaponlog_description_component'] = '%s: %s on/from %s (%s)',
+    ['weaponlog_description_ammo'] = '%s: %sx Ammunition to the weapon %s (%s)',
+    ['weaponlog_description_tint'] = '%s: %s on the weapon %s (%s)',
+    ['weaponlog_description_nothing'] = '%s: %s (%s)',
+
+    ------ Drug Log ------
+    ['druglog_description_sell'] = '%s: %sx %s (%s) for $%s Black money',
+    ['druglog_description'] = '%s: %sx %s (%s)',
+
+    ------ Ambulance Log ------
+    ['ambulancelog_revive_for_money'] = '%s: %s für %s',
+    ['ambulancelog_description'] = '%s: %s',
+
+    ------ Society Log ------
+    ['societylog_description'] = '%s | %s: $%s',
+}
